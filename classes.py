5ebe6d83d80198dca8b345133fb24e1b98d970e9
Andrew <andrew.bingham@oit.edu>
2015-12-11 21:10:37 -0800

    still unsure of program structure

--- classes.py
+++ classes.py
@@ -1,5 +1,11 @@
+
 class Player:
     def __init__(self,name):
         self.name = name
         self.weapon = str()
         self.items = []
+
+class Map:
+    def __init__(self,size):
+        self.size = size
+
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from classes import Player
+from classes import *
+from functions import *
+import time                     #required for sleep function
 
-print("You are about to embark on an adventure!\n"
-      "In text!\n\n")
+
+print("You are about to embark on an adventure!")
+for i in range(5):
+    print(".")
+    time.sleep(1)
+
+print("In text!\n\n")
+time.sleep(5)
+clearConsole()
+
+print("Before you embark I need to know a little bit about you...")
 
 player1 = Player(input("What is your name? "))
-
 player1.weapon = input(player1.name + ", what is your weapon of choice? ")
+
+input("")
