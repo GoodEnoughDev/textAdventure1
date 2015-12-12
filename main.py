aa50b7643638336eab7375e6a8f0ecffd414816d
Andrew <andrew.bingham@oit.edu>
2015-12-11 20:40:43 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from textAdventureClasses import Player
+
+print("You are about to embark on an adventure!\n"
+      "In text!\n\n")
+
+player1 = Player(input("What is your name? "))
+
+player1.weapon = input(player1.name + ", what is your weapon of choice? ")
